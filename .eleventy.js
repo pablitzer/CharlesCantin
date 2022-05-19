2a200b36d4a4b2c45c90022f15193ad7af68a8dc
Pascal Ablitzer <pascal.ablitzer@gmail.com>
2022-05-19 13:43:36 +0200

    Add configuration folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./admin');
   eleventyConfig.addPassthroughCopy('./images');
   eleventyConfig.addPassthroughCopy('./javascript');
+  eleventyConfig.addPassthroughCopy('./configuration');
   // Return your Object options:
   return {
     dir: {
